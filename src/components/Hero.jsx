3d47ba4840b0b17e9c969d1608ee799ab87b9eed
Ndifreke Etukudo <ndifreke.etukudo@decagon.dev>
2023-03-15 19:39:50 +0100

    computer image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,17 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className=" w- [35px] h-[64px] rounded-3x1 border-secondary flex justify-center items-start p-2">
+            <motion.dev 
+            animate={{
+              y:[0,24,0]
+            }}
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
